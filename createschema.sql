e324d957fd514dc6c34da0fa16e0cda690d0ad63
jamesSolum <james.n.solum@gmail.com>
2018-04-20 16:06:04 -0700

    update

--- createschema.sql
+++ createschema.sql
@@ -1,3 +1,11 @@
+CREATE SEQUENCE job_id_seq; --For autoincrement of jobid--
+CREATE TABLE job(
+	jobid integer NOT NULL DEFAULT nextval('job_id_seq') PRIMARY KEY,
+	role varchar(255),
+	salary decimal(19, 2), -- assume nothing will be higher than 19 digits -- 
+	industry varchar(255)
+);
+
 /*
 	Create the Schemas for the Student Interests and Careers Database
 */
@@ -7,16 +15,8 @@ CREATE TABLE student(
 	sex char(1) CHECK (sex = 'M' OR sex = 'F'), -- assume only male and female --
 	email varchar(255) CHECK (email like '%_@_%._%') PRIMARY KEY,
 	gpa decimal(3, 2),
-	dreamJob integer,
+	dreamJob integer references job(jobid),
 	CHECK (length(email) > length(lastname) + length(firstname)) -- we want school emails, which will be a combination of first and lastname-- 	
-);
-
-CREATE SEQUENCE job_id_seq; --For autoincrement of jobid--
-CREATE TABLE job(
-	jobid integer NOT NULL DEFAULT nextval('job_id_seq') PRIMARY KEY,
-	role varchar(255),
-	salary decimal(19, 2), -- assume nothing will be higher than 19 digits -- 
-	industry varchar(255)
 );
 
 CREATE TABLE hobby(
@@ -34,25 +34,25 @@ CREATE TABLE class(
 );
 
 CREATE TABLE employedBy(
-	email varchar(255) references student(email),
+	email varchar(255) references student(email) ON DELETE CASCADE,
 	jobid integer references job(jobid),
 	PRIMARY KEY (email, jobid) 
 );
 
 CREATE TABLE participates(
-	email varchar(255) references student(email), 
+	email varchar(255) references student(email) ON DELETE CASCADE, 
 	hobbyName varchar(255) references hobby(hobbyName),
 	PRIMARY KEY (email, hobbyName)
 );
 
 CREATE TABLE studies(
-	email varchar(255) references student(email),
+	email varchar(255) references student(email) ON DELETE CASCADE,
 	subjectName varchar(255) references subject(subjectName),
 	PRIMARY KEY (email, subjectName)
 );
 
 CREATE TABLE takes(
-	email varchar(255) references student(email),
+	email varchar(255) references student(email) ON DELETE CASCADE,
 	className varchar(255) references class(className),
 	PRIMARY KEY (email, className)
 );
